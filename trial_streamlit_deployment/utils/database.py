3fea8182e3a5085e2c0a02d3e0ac2780a045566c
luke@neoalphaglobal.com <lukellanillo@gmail.com>
2024-07-15 16:46:37 +0800

    chore: Add port address

--- trial_streamlit_deployment/utils/database.py
+++ trial_streamlit_deployment/utils/database.py
@@ -5,6 +5,7 @@ def get_connection():
     return mysql.connector.connect(
         host=st.secrets["mysql"]["host"],
         user=st.secrets["mysql"]["user"],
+        port = st.secrets["mysql"]["port"],
         password=st.secrets["mysql"]["password"],
         database=st.secrets["mysql"]["database"]
     )
