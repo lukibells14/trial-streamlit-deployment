8faf2f913515e41465951e2957501dac1161f14f
luke@neoalphaglobal.com <lukellanillo@gmail.com>
2024-07-15 16:57:33 +0800

    chore: Update database connections

--- trial_streamlit_deployment/Homepage.py
+++ trial_streamlit_deployment/Homepage.py
@@ -1,32 +1,14 @@
 # specsheet/Homepage.py
+# streamlit_app.py
+
 import streamlit as st
-from utils.database import get_connection
 
-def main():
-    st.title("Product Viewer")
+# Initialize connection.
+conn = st.connection('mysql', type='sql')
 
-    # Test accessing secrets
-    try:
-        st.write("Host:", st.secrets["mysql"]["host"])
-        st.write("User:", st.secrets["mysql"]["user"])
-        st.write("Database:", st.secrets["mysql"]["database"])
-    except Exception as e:
-        st.error(f"Failed to access secrets: {e}")
+# Perform query.
+df = conn.query('SELECT * from mytable;', ttl=600)
 
-    # Test database connection
-    try:
-        conn = get_connection()
-        cursor = conn.cursor(dictionary=True)
-        cursor.execute("SELECT * FROM Product")
-        products = cursor.fetchall()
-        conn.close()
-
-        st.success("Connected to the database successfully!")
-        st.write("Products:")
-        for product in products:
-            st.write(product)
-    except Exception as e:
-        st.error(f"Failed to connect to the database: {e}")
-
-if __name__ == "__main__":
-    main()
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
