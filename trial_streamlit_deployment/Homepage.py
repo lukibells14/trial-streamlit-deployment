fc36902cf346a6e80a04a7283cc58fe81a3f06a6
luke@neoalphaglobal.com <lukellanillo@gmail.com>
2024-07-15 16:22:42 +0800

    chore: Reset modules

--- trial_streamlit_deployment/Homepage.py
+++ trial_streamlit_deployment/Homepage.py
@@ -1,8 +1,17 @@
+# specsheet/Homepage.py
 import streamlit as st
 from utils.database import get_connection
 
 def main():
     st.title("Product Viewer")
+
+    # Test accessing secrets
+    try:
+        st.write("Host:", st.secrets["mysql"]["host"])
+        st.write("User:", st.secrets["mysql"]["user"])
+        st.write("Database:", st.secrets["mysql"]["database"])
+    except Exception as e:
+        st.error(f"Failed to access secrets: {e}")
 
     # Test database connection
     try:
